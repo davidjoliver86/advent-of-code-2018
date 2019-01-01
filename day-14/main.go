5d81e0fc2e41cc83b99cb020ec172fe1cf46d728
davidjoliver86 <davidjoliver86@gmail.com>
2019-01-01 12:01:07 -0800

    day 14

--- day-14/main.go
+++ day-14/main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// Initial input was "030121"
+// The first star expects us to use "030121" as an integer - that is: "30,121 iterations"
+// The second star expects a sequence of {0, 3, 0, 1, 2, 1}
+// Also the initial two recipes (3, 7) are considered constant
 const inputIterations = 30121
 
 var initialRecipes = []int{3, 7}
 var inputSequence = []int{0, 3, 0, 1, 2, 1}
 
+// Elf is a container with an index that points to the recipe array
 type Elf struct {
 	// Current is the index of the recipe array
 	id      int
@@ -17,11 +24,13 @@ func (e *Elf) String() string {
 	return fmt.Sprintf("Elf[%d]=>(%d)", e.id, e.Current)
 }
 
+// Score deferences the recipe array at the Elf's Current location
 func (e *Elf) Score(r *[]int) int {
 	score := (*r)[e.Current]
 	return score
 }
 
+// AddRecipe combines the Scores of the teo Elfs' recipes and adds each digit to the array of recipe scores
 func AddRecipe(recipes *[]int, e1 *Elf, e2 *Elf) {
 	sum := e1.Score(recipes) + e2.Score(recipes)
 	if sum >= 10 {
@@ -37,7 +46,33 @@ func AddRecipe(recipes *[]int, e1 *Elf, e2 *Elf) {
 	}
 }
 
-func main() {
+// FindSequence continually adds recipes until seq appears at the tail-end of the recipe array, then returns
+// the number of recipes before that (in other words, the index *of* seq's first appearance)
+func FindSequence(recipes *[]int, e1 *Elf, e2 *Elf, seq []int) int {
+	counter := 0
+	index := 0
+	sLen := len(seq)
+	for {
+		AddRecipe(recipes, e1, e2)
+		// compare consecutive digits from recipes[index] onward
+		// as long as digits match, increment the couter
+		// once they dont match, reset counter to 0
+		for i, v := range seq {
+			if (*recipes)[index+i] == v {
+				counter++
+				if counter == sLen {
+					return index
+				}
+			} else {
+				index++
+				counter = 0
+				break
+			}
+		}
+	}
+}
+
+func firstStar() {
 	// first star
 	recipes := initialRecipes[:]
 	e1 := Elf{0, 0}
@@ -46,9 +81,18 @@ func main() {
 		AddRecipe(&recipes, &e1, &e2)
 	}
 	fmt.Println(recipes[inputIterations:])
+}
 
+func secondStar() {
 	// secondstar
 	recipes := initialRecipes[:]
 	e1 := Elf{0, 0}
 	e2 := Elf{1, 1}
+	seq := FindSequence(&recipes, &e1, &e2, inputSequence)
+	fmt.Println(seq)
+}
+
+func main() {
+	firstStar()
+	secondStar()
 }
--- day-14/main_test.go
+++ day-14/main_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+type findSequenceTestCase struct {
+	input    []int
+	expected int
+}
+
+var findSequenceTestCases = []findSequenceTestCase{
+	{[]int{5, 1, 5, 8, 9}, 9},
+	{[]int{0, 1, 2, 4, 5}, 5},
+	{[]int{9, 2, 5, 1, 0}, 18},
+	{[]int{5, 9, 4, 1, 4}, 2018},
+}
+
 func TestAddRecipe(t *testing.T) {
 	recipes := []int{3, 7}
 	e1 := Elf{0, 0}
@@ -15,5 +27,18 @@ func TestAddRecipe(t *testing.T) {
 	result := fmt.Sprintf("%v", recipes[9:])
 	if result != "[5 1 5 8 9 1 6 7 7 9]" {
 		t.Errorf("Expected [5 1 5 8 9 1 6 7 7 9], got %v", result)
+	}
+}
+
+func TestFindSequence(t *testing.T) {
+	for _, testcase := range findSequenceTestCases {
+		recipes := []int{3, 7}
+		e1 := Elf{0, 0}
+		e2 := Elf{1, 1}
+		seq := FindSequence(&recipes, &e1, &e2, testcase.input)
+		fmt.Println(seq)
+		if seq != testcase.expected {
+			t.Errorf("Expected to find %v at %d, got %d", testcase.input, testcase.expected, seq)
+		}
 	}
 }
